db21480b9b7071dfa1744aeb774a2e5878d09905
burn25 <burnham_jeremy@hotmail.com>
2018-08-22 21:08:33 -0400

    convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jeremy Burnham on 8/22/18.
+//  Copyright © 2018 Jeremy Burnham. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
